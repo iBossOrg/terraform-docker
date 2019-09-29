76a45bc25b5bf6bfb7f751911c1d99e0aff85f54
Petr Řehoř <rx@rx.cz>
2019-09-30 01:31:00 +0200

    Add the 'id' variable to k3s module output
    Signed-off-by: Petr Řehoř <rx@rx.cz>

--- k3s/output.tf
+++ k3s/output.tf
@@ -1,3 +1,9 @@
+output "id" {
+  description = "Docker k3s master node container ids"
+  value       = docker_container.k3s_master.id
+}
+
 output "kubeconfig_path" {
-  value = "${abspath(var.kubeconfig_dir)}/kubeconfig.yaml"
+  description = "Path to kubeconfig.yaml"
+  value       = "${abspath(var.kubeconfig_dir)}/kubeconfig.yaml"
 }
--- k3s/test/test.tf
+++ k3s/test/test.tf
@@ -7,6 +7,10 @@ module "k3s_single" {
   kubernetes_api_port = 6442
 }
 
+output "k3s_single_id" {
+  value = module.k3s_single.id
+}
+
 output "k3s_single_kubeconfig_path" {
   value = module.k3s_single.kubeconfig_path
 }
@@ -18,6 +22,10 @@ module k3s_cluster {
   cluster_name       = "k3s-cluster"
   kubeconfig_dir     = "k3s-cluster"
   worker_nodes_count = 3
+}
+
+output "k3s_cluster_id" {
+  value = module.k3s_cluster.id
 }
 
 output "k3s_cluster_kubeconfig_path" {
